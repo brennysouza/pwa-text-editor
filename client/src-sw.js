70ae97cfbad0dce090315c04d94ad3e1eb61c936
Brenny Souza <106251815+brennysouza@users.noreply.github.com>
2023-10-20 18:53:16 -0400

    added stalewhilerevalidate to src file, might be issue as to why app isnt fully functioning on heroku

--- client/src-sw.js
+++ client/src-sw.js
@@ -4,6 +4,7 @@ const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
 const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
+const { StaleWhileRevalidate } = require('workbox-strategies');
 
 precacheAndRoute(self.__WB_MANIFEST);
 
@@ -30,17 +31,33 @@ registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 registerRoute(
   ({ request }) =>
     request.destination === 'style' || request.destination === 'script' || request.destination === 'worker' || request.destination === 'image',
+    new StaleWhileRevalidate({
+      cacheName: 'asset-cache',
+      plugins: [
+        new CacheableResponsePlugin({
+          statuses: [0, 200],
+        }),
+        new ExpirationPlugin({
+          maxAgeSeconds: 30 * 24 * 60 * 60,
+        }),
+      ],
+    })
+    );
     // This cachefirst strategy will cache the assets and serve them from the cache if available.
     // Variable above is defined as CacheFirst.
-  new CacheFirst({
-    cacheName: 'asset-cache',
-    plugins: [
-      new CacheableResponsePlugin({
-        statuses: [0, 200],
-      }),
-      new ExpirationPlugin({
-        maxAgeSeconds: 30 * 24 * 60 * 60,
-      }),
-    ],
-  })
-);
+    
+//   new CacheFirst({
+//     cacheName: 'asset-cache',
+//     plugins: [
+//       new CacheableResponsePlugin({
+//         statuses: [0, 200],
+//       }),
+//       new ExpirationPlugin({
+//         maxAgeSeconds: 30 * 24 * 60 * 60,
+//       }),
+//     ],
+//   })
+// );
+
+
+
