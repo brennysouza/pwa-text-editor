0f5c80796dd635796fafc877582de742e8d2a3cc
Brenny Souza <106251815+brennysouza@users.noreply.github.com>
2023-10-20 17:52:11 -0400

    added icons with various sizes to dist folder

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -48,7 +48,7 @@ module.exports = () => {
           {
             src: path.resolve("src/images/logo.png"),
             // May need to add more sizes for logo/icon
-            sizes: [128],
+            sizes: [90, 130, 190, 260, 390, 500],
             destination: path.join("assets", "icons"),
           },
         ],
