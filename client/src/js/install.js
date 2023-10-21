fa746a857d863136dedc219987665d9b0f14d8cb
Brenny Souza <106251815+brennysouza@users.noreply.github.com>
2023-10-21 14:32:06 -0400

    added console logs to event listeners in install.js file in src folder

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -3,34 +3,35 @@ const butInstall = document.getElementById('buttonInstall');
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
-    // Prevents the default browser prompt to install the PWA
-    window.deferredPrompt = event;
-    // event.preventDefault(); 
-    // deferredPrompt = event;
-    butInstall.classList.toggle('hidden', false);
+  // Prevents the default browser prompt to install the PWA
+  window.deferredPrompt = event;
+  // event.preventDefault(); 
+  // deferredPrompt = event;
+  butInstall.classList.toggle('hidden', false);
+  console.log('beforeinstallprompt fired');
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
-    // installation prompt
-    if (deferredPrompt) {
-        deferredPrompt.prompt();
-        const choiceResult = await deferredPrompt.userChoice;
-    
-        if (choiceResult.outcome === 'accepted') {
-          console.log('User accepted the PWA installation');
-        }
-        // Reset the deferred prompt variable to null as it can only be used once.
-        
-        deferredPrompt = null; 
-        butInstall.classList.toggle('hidden', true);
-      }
-    
+  // installation prompt
+  if (deferredPrompt) {
+    deferredPrompt.prompt();
+    const choiceResult = await deferredPrompt.userChoice;
+
+    if (choiceResult.outcome === 'accepted') {
+      console.log('User accepted the PWA installation');
+    }
+    // Reset the deferred prompt variable to null as it can only be used once.
+
+    deferredPrompt = null;
+    butInstall.classList.toggle('hidden', true);
+  }
+  console.log('butInstall clicked');
 });
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
-    console.log('App has been installed.');
-    window.deferredPrompt = null;
-
+  console.log('App has been installed.');
+  window.deferredPrompt = null;
+  console.log('appinstalled fired');
 });
