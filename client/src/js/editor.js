f109acc9a8fcb4c41980353c6a87e0f6d05c5879
Brenny Souza <106251815+brennysouza@users.noreply.github.com>
2023-10-20 20:45:48 -0400

    revised code in editor js file for header to appear, testing to see if works

--- client/src/js/editor.js
+++ client/src/js/editor.js
@@ -1,6 +1,6 @@
 // Import methods to save and get data from the indexedDB database in './database.js'
 import { getDb, putDb } from './database';
-import { header } from './header';
+import { header } from './src/js/header';
 
 export default class {
   constructor() {
